72db184f13bd271e0d165f079d02921054519357
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2021-01-19 19:50:07 +0100

    update config handling

--- src/saiive.defi.api/Program.cs
+++ src/saiive.defi.api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace saiive.defi.api
@@ -12,6 +13,10 @@ namespace saiive.defi.api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddEnvironmentVariables();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>().UseUrls("http://+:5000");
