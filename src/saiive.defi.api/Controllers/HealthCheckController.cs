11b6aa1cb59b1722b96b1f67417972bab7007918
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2021-01-19 19:58:02 +0100

    add more logs

--- src/saiive.defi.api/Controllers/HealthCheckController.cs
+++ src/saiive.defi.api/Controllers/HealthCheckController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace saiive.defi.api.Controllers
 {
@@ -6,10 +8,29 @@ namespace saiive.defi.api.Controllers
     [Route("v1/api/health")]
     public class HealthCheckController : ControllerBase
     {
-        
+        private readonly IConfiguration _config;
+        private readonly ILogger _logger;
+
+        public HealthCheckController(IConfiguration config, ILogger<HealthCheckController> logger)
+        {
+            _config = config;
+            _logger = logger;
+        }
         [HttpGet]
         public bool HealthCheck()
         {
+            return true;
+        }
+
+        [HttpGet("test")]
+        public bool Test()
+        {
+            var root = (IConfigurationRoot)_config;
+            var debugView = root.GetDebugView();
+
+            _logger.LogInformation(debugView);
+
+
             return true;
         }
         
