d592eb0f6478ae4279193d4cb732d7073997ad21
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2021-01-20 21:03:55 +0100

    update super-node api

---
+++ terraform/libs/frontdoor/variables.tf
@@ -0,0 +1,28 @@
+variable "environment" {
+}
+
+variable "prefix" {
+}
+
+variable "resource_group" {
+	
+}
+variable "location" {
+	
+}
+variable "name" {
+	
+}
+
+
+
+variable "dns_zone" {
+  
+}
+variable "dns_zone_resource_group" {
+
+}
+
+variable "scaleway_nodes" {
+    
+}
--- terraform/libs/traffic_manager/output.tf
+++ terraform/libs/traffic_manager/output.tf
@@ -1,3 +1,11 @@
 output "name" {
   value = azurerm_traffic_manager_profile.traffic_manager.name
 }
+
+output "cname" {
+  value = azurerm_dns_cname_record.traffic_manager_cname.name
+}
+
+output "public_endpoint" {
+  value = "${azurerm_dns_cname_record.traffic_manager_cname.name}.${var.dns_zone}"
+}
