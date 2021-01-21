8002bd9c78f1ea4c6588f5d3334a20f484c570b6
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2021-01-21 20:49:15 +0100

    implement new methods

---
+++ src/saiive.defi.api/Controllers/TokenController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using saiive.defi.api.Model;
+
+namespace saiive.defi.api.Controllers
+{
+    [ApiController]
+    [Route("v1/api/")]
+    public class TokenController : BaseController
+    {
+        public TokenController(ILogger<TokenController> logger, IConfiguration config) : base(logger, config)
+        {
+          
+        }
+
+
+        [HttpGet("{coin}/tokens")]
+        public async Task<IActionResult> ListTokens(string coin)
+        {
+            var response = await _client.GetAsync($"{ApiUrl}/api/{coin}/{Network}/token/list");
+
+            try
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                response.EnsureSuccessStatusCode();
+
+                
+                return Ok(data);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError($"{e}");
+                return BadRequest(e);
+            }
+        }
+
+        [HttpGet("{coin}/tokens/{token}")]
+        public async Task<IActionResult> GetTokens(string coin, string token)
+        {
+            var response = await _client.GetAsync($"{ApiUrl}/api/{coin}/{Network}/token/get/{token}");
+
+            try
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                response.EnsureSuccessStatusCode();
+                
+                return Ok(data);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError($"{e}");
+                return BadRequest(e);
+            }
+        }
+
+    }
+}
