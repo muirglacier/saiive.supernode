044dd5b22caa5d0b867384d15b1bd6588e1c2a82
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2021-01-21 20:57:20 +0100

    implement new methods

---
+++ src/saiive.defi.api/Model/AccountModel.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace saiive.defi.api.Model
+{
+    public class AccountModel
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("balance")]
+        public double Balance { get; set; }
+
+        [JsonProperty("raw")]
+        public string Raw { get; set; }
+    }
+}
