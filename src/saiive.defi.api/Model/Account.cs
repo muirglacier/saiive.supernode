60d24171172cd873924f4d8232ea1aba8b01d761
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2021-01-23 19:29:43 +0100

    update accounts response

---
+++ src/saiive.defi.api/Model/Account.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace saiive.defi.api.Model
+{
+    public class Account
+    {
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("accounts")]
+        public IList<AccountModel> Accounts { get; set; }
+    }
+}
