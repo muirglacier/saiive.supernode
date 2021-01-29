a9b8055650cf4d376ab94651534f1258d10441bc
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2021-01-29 19:23:24 +0100

    add network to request url

---
+++ src/saiive.defi.api/Model/TransactionResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace saiive.defi.api.Model
+{
+    public class TransactionRequest
+    {
+        [JsonProperty("rawTx")]
+        public string RawTx { get; set; }
+    }
+
+    public class TransactionResponse
+    {
+        [JsonProperty("txid")]
+        public string TxId { get; set; }
+    }
+}
