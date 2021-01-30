f910913586572d4cb1f3e7f262ea412ba8924cbc
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2021-01-30 10:31:29 +0100

    add defichain api controller

---
+++ src/saiive.defi.api/Model/YieldFarmingModelRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace saiive.defi.api.Model
+{
+    public class YieldFramingModelRequest
+    {
+        [JsonProperty("pools")]
+        public List<YieldFramingModel> pools { get; set; }
+    }
+}
