73134bfd667f9b915eeaa3414566d7163c23f3a9
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2021-01-31 20:26:56 +0100

    add test poolswap api

---
+++ src/saiive.defi.api/Model/TestPoolSwapModel.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace saiive.defi.api.Model
+{
+    public class TestPoolSwapModel
+    {
+        [JsonProperty("result")]
+        public string Result { get; set; }
+    }
+}
---
+++ src/saiive.defi.api/Requests/TestPoolSwapBodyRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace saiive.defi.api.Requests
+{
+    public class TestPoolSwapBodyRequest
+    {
+        [JsonProperty("from")]
+        public string From{ get; set; }
+
+        [JsonProperty("tokenFrom")]
+        public string TokenFrom { get; set; }
+
+        [JsonProperty("amountFrom")]
+        public double AmountFrom { get; set; }
+
+        [JsonProperty("to")]
+        public string To { get; set; }
+
+        [JsonProperty("tokenTo")]
+        public string TokenTo { get; set; }
+
+        [JsonProperty("maxPrice")]
+        public double MaxPrice { get; set; }
+    }
+}
