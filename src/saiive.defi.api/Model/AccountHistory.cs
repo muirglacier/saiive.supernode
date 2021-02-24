30f23e03b14770f7b709295672cfea6676803c9d
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2021-02-24 22:10:01 +0100

    account history api

---
+++ src/saiive.defi.api/Model/AccountHistory.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace saiive.defi.api.Model
+{
+    public class AccountHistory
+    {
+        [JsonProperty("owner")]
+        public string Owner { get; set; }
+
+        [JsonProperty("blockHeight")]
+        public int BlockHeight { get; set; }
+
+        [JsonProperty("blockHash")]
+        public string BlockHash { get; set; }
+
+        [JsonProperty("blockTime")]
+        public int BlockTime { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("poolID")]
+        public string PoolID { get; set; }
+
+        [JsonProperty("amounts")]
+        public IList<string> Amounts { get; set; }
+    }
+}
