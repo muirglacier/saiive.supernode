8f8bd5ad4e8de61359cd8e01d5c0cb170e7877eb
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2021-03-15 19:10:04 +0100

    update healthcheck to ensure bitcore is running

--- src/saiive.defi.api/Controllers/HealthCheckController.cs
+++ src/saiive.defi.api/Controllers/HealthCheckController.cs
@@ -1,18 +1,22 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using saiive.defi.api.Model;
 
 namespace saiive.defi.api.Controllers
 {
     [ApiController]
     [Route("v1/api/health")]
-    public class HealthCheckController : ControllerBase
+    public class HealthCheckController : BaseController
     {
         private readonly IConfiguration _config;
         private readonly ILogger _logger;
 
-        public HealthCheckController(IConfiguration config, ILogger<HealthCheckController> logger)
+        public HealthCheckController(IConfiguration config, ILogger<HealthCheckController> logger) : base(logger, config)
         {
             _config = config;
             _logger = logger;
@@ -20,9 +24,21 @@ namespace saiive.defi.api.Controllers
         [HttpGet]
 
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public IActionResult HealthCheck()
+        public async Task<IActionResult> HealthCheck()
         {
-            return NoContent();
+            var response = await _client.GetAsync($"{ApiUrl}/api/status/enabled-chains");
+
+            try
+            {
+                response.EnsureSuccessStatusCode();
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError($"{e}");
+                return BadRequest(new ErrorModel(e.Message));
+            }
         }
 
     }
