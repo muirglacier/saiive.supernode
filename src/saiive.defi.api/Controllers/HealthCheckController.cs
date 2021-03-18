0aabd27ca9e26d3a5f7b20f3ec6bc5a06f0c4aab
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2021-03-18 12:06:28 +0100

    update healthcheck

--- src/saiive.defi.api/Controllers/HealthCheckController.cs
+++ src/saiive.defi.api/Controllers/HealthCheckController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using saiive.defi.api.Model;
 
 namespace saiive.defi.api.Controllers
@@ -15,18 +17,47 @@ namespace saiive.defi.api.Controllers
         public HealthCheckController(IConfiguration config, ILogger<HealthCheckController> logger) : base(logger, config)
         {
         }
-        
+
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<IActionResult> HealthCheck()
+        public IActionResult HealthCheck()
         {
-            var response = await _client.GetAsync($"{ApiUrl}/api/status/enabled-chains");
+            return NoContent();
+        }
+        
+        [HttpGet("{coin}/{network}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> HealthCheckNetwork(string network, string coin)
+        {
+            var response = await _client.GetAsync($"{ApiUrl}/api/{coin}/{network}/block/tip");
 
             try
             {
                 response.EnsureSuccessStatusCode();
 
-                return NoContent();
+                if (response.StatusCode == HttpStatusCode.NoContent)
+                {
+                    throw new ArgumentException("Node is not synced or running...");
+                }
+
+                var data = await response.Content.ReadAsStringAsync();
+
+                var obj = JsonConvert.DeserializeObject<BlockModel>(data);
+
+                if (obj == null)
+                {
+                    throw new ArgumentException("block model is empty");
+                }
+                var time = Convert.ToDateTime(obj.Time);
+                var timeStartCheck = DateTime.Now.AddHours(-5);
+                var timeEndCheck = DateTime.Now.AddHours(5);
+
+                if (time >= timeStartCheck && time <= timeEndCheck)
+                {
+                    return Ok(obj);
+                }
+
+                return BadRequest("Chain is not synced yet!");
             }
             catch (Exception e)
             {
