f9338af81b531c4176d884526d6f43edfedcf297
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2021-06-13 12:04:12 +0200

    add masternode controller

---
+++ src/Saiive.Supernode/Controllers/MasternodeController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Saiive.SuperNode.Model;
+
+namespace Saiive.SuperNode.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/")]
+    public class MasternodeController : BaseController
+    {
+        private Dictionary<string, List<Masternode>> _cachedList;
+        private DateTime? _lastRefreshTime;
+        private const string NULL_TX_ID = "0000000000000000000000000000000000000000000000000000000000000000";
+
+        public MasternodeController(ILogger<MasternodeController> logger, IConfiguration config) : base(logger, config)
+        {
+            _cachedList = new Dictionary<string, List<Masternode>>();
+        }
+
+        private async Task UpdateCachedList(string coin, string network)
+        {
+            var response = await _client.GetAsync($"{ApiUrl}/api/{coin}/{network}/masternodes/list");
+
+            try
+            {
+                response.EnsureSuccessStatusCode();
+
+                var data = await response.Content.ReadAsStringAsync();
+
+                var masterNodeList = JsonConvert.DeserializeObject<Dictionary<string, Masternode>>(data);
+
+                var ret = new List<Masternode>();
+
+                foreach (var masternode in masterNodeList)
+                {
+                    masternode.Value.Id = masternode.Key;
+                    ret.Add(masternode.Value);
+                }
+
+                _lastRefreshTime = DateTime.UtcNow;
+
+                if (!_cachedList.ContainsKey(network))
+                {
+                    _cachedList.Add(network, ret);
+                }
+                else
+                {
+                    _cachedList[network] = ret;
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Logger.LogError($"{e}");
+            }
+        }
+
+
+        [HttpGet("{network}/{coin}/list")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Masternode>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ErrorModel))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ErrorModel))]
+        public async Task<IActionResult> ListMasternodes(string coin, string network)
+        {
+
+            try
+            {
+                if (!_cachedList.ContainsKey(coin) ||
+                    _cachedList[coin].Count == 0 ||
+                    _lastRefreshTime == null ||
+                    (DateTime.UtcNow - _lastRefreshTime.Value) > TimeSpan.FromDays(1))
+                {
+                    await UpdateCachedList(coin, network);
+                }
+
+
+                return Ok(_cachedList[network]);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError($"{e}");
+                return BadRequest(new ErrorModel(e.Message));
+            }
+        }
+
+        [HttpGet("{network}/{coin}/list/active")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Masternode>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ErrorModel))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ErrorModel))]
+        public async Task<IActionResult> ListActiveMasternodes(string coin, string network)
+        {
+
+            try
+            {
+                if (!_cachedList.ContainsKey(coin) ||
+                    _cachedList[coin].Count == 0 ||
+                    _lastRefreshTime == null ||
+                    (DateTime.UtcNow - _lastRefreshTime.Value) > TimeSpan.FromDays(1))
+                {
+                    await UpdateCachedList(coin, network);
+                }
+
+                var retList = _cachedList[network].Where(a => a.ResignTx == NULL_TX_ID);
+                return Ok(retList);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError($"{e}");
+                return BadRequest(new ErrorModel(e.Message));
+            }
+        }
+
+
+    }
+}
---
+++ src/Saiive.Supernode/Model/Masternode.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+
+namespace Saiive.SuperNode.Model
+{
+    public class Masternode
+    {
+        [JsonProperty("id")] 
+        public string Id { get; set; }
+
+        [JsonProperty("ownerAuthAddress")] 
+        public string OwnerAuthAddress { get; set; }
+
+        [JsonProperty("operatorAuthAddress")] 
+        public string OperatorAuthAddress { get; set; }
+
+        [JsonProperty("creationHeight")] 
+        public int CreationHeight { get; set; }
+
+        [JsonProperty("resignHeight")] 
+        public int ResignHeight { get; set; }
+
+        [JsonProperty("resignTx")] 
+        public string ResignTx { get; set; }
+
+        [JsonProperty("banHeight")] 
+        public int BanHeight { get; set; }
+
+        [JsonProperty("banTx")] 
+        public string BanTx { get; set; }
+
+        [JsonProperty("state")] 
+        public string State { get; set; }
+
+        [JsonProperty("mintedBlocks")] 
+        public int MintedBlocks { get; set; }
+
+        [JsonProperty("ownerIsMine")] 
+        public bool OwnerIsMine { get; set; }
+
+        [JsonProperty("operatorIsMine")] 
+        public bool OperatorIsMine { get; set; }
+
+        [JsonProperty("localMasternode")] 
+        public bool LocalMasternode { get; set; }
+    }
+}
