92dc0f80d549cac487f4b046d8f7310daa0baf68
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2021-06-17 13:02:17 +0200

    fix startup

--- src/Saiive.Supernode/Cache/MasterNodeCacheStartupHandler.cs
+++ src/Saiive.Supernode/Cache/MasterNodeCacheStartupHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -10,18 +11,28 @@ namespace Saiive.SuperNode.Cache
     {
         private readonly IMasterNodeCache _cache;
         private readonly ILogger<MasterNodeCacheStartupHandler> _logger;
+        private readonly IConfiguration _config;
         private Timer _timer;
 
-        public MasterNodeCacheStartupHandler(IMasterNodeCache cache, ILogger<MasterNodeCacheStartupHandler> logger)
+        public MasterNodeCacheStartupHandler(IMasterNodeCache cache, ILogger<MasterNodeCacheStartupHandler> logger, IConfiguration config)
         {
             _cache = cache;
             _logger = logger;
+            _config = config;
         }
 
         private async Task UpdateCache()
         {
             try
             {
+                var defichainConfig = _config["DEFICHAIN"];
+                var defichainEnabled = Convert.ToBoolean(String.IsNullOrEmpty(defichainConfig) ? "true" : defichainConfig);
+                if (!defichainEnabled)
+                {
+                    _logger.LogInformation("DeFiChain not enabled on this node!...");
+                    return;
+                }
+
                 _logger.LogInformation("Update masternode cache...");
                 var mainnet = _cache.GetMasterNodes("mainnet", "DFI");
                 var testnet = _cache.GetMasterNodes("testnet", "DFI");
