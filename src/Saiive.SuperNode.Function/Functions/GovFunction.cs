5ae28197c4360a87c3951f8bf7d93e873420f3cf
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2021-09-21 13:31:30 +0200

    add gov function

---
+++ src/Saiive.SuperNode.Function/Functions/GovFunction.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Saiive.SuperNode.Abstaction;
+using Saiive.SuperNode.Model;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Saiive.SuperNode.Function.Functions
+{
+    public class GovFunction : BaseFunction
+    {
+        private readonly Dictionary<string, double> _blockchainTimeCheckMinuteInterval;
+        private const double DefaultCheckMinuteInterval = 300;
+
+        public GovFunction(ILogger<AddressFunctions> logger, ChainProviderCollection chainProviderCollection, IServiceProvider serviceProvider) : base(logger, chainProviderCollection, serviceProvider)
+        {
+            _blockchainTimeCheckMinuteInterval = new Dictionary<string, double>();
+            _blockchainTimeCheckMinuteInterval.Add("BTC", DefaultCheckMinuteInterval);
+            _blockchainTimeCheckMinuteInterval.Add("DFI", TimeSpan.FromMinutes(60).TotalMinutes);
+        }
+
+        [FunctionName("Gov")]
+        [OpenApiOperation(operationId: "Gov", tags: new[] { "Gov" })]
+        [OpenApiParameter(name: "network", In = ParameterLocation.Path, Required = true, Type = typeof(string))]
+        [OpenApiParameter(name: "coin", In = ParameterLocation.Path, Required = true, Type = typeof(string))]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(object), Description = "The OK response")]
+        public async Task<IActionResult> GetGov(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v1/{network}/{coin}/gov")] HttpRequestMessage req,
+            string network, string coin,
+            ILogger log)
+        {
+            try
+            {
+                return new OkObjectResult("");
+            }
+            catch (Exception e)
+            {
+                Logger.LogError($"{e}", e);
+                return new BadRequestObjectResult(new ErrorModel(e.Message));
+            }
+        }
+    }
+
+}
