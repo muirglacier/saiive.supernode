dd6becc603b25ebd915cfdc72e7b228620820576
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2021-10-11 22:02:07 +0200

    change swagger tos and title

---
+++ src/Saiive.SuperNode.Function/OpenApiDescription.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace Saiive.SuperNode.Function
+{
+    public class OpenApiConfigurationOptions : DefaultOpenApiConfigurationOptions
+    {
+
+        public override OpenApiInfo Info { get; set; } = new OpenApiInfo()
+        {
+            Version = "1.0.0",
+            Title = "saiive.live DeFi API",
+            Description = "saiive.live DeFi Wallet API for DeFiChain & Bitcoin.",
+            TermsOfService = new Uri("https://static.saiive.live/tos.html")
+        };
+
+    }
+}
